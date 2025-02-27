7ba332ed6205088673dc46a35aac736b8a03cf3f
Dmytro Kulish <mitiakylish2@gmail.com>
2025-02-27 21:42:53 +0200

    Added FilesTest

---
+++ src/test/java/FilesTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class FilesTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+}
