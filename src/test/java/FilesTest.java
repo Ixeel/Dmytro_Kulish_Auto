81196a94ac593001b183568b5b1183272d280188
Dmytro Kulish <mitiakylish2@gmail.com>
2025-02-27 22:30:26 +0200

    Added all files for FileTest

--- src/test/java/FilesTest.java
+++ src/test/java/FilesTest.java
@@ -1,7 +1,15 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import patterns.pageobject.GaragePage;
+import patterns.pageobject.HomePage;
+import patterns.pageobject.InstructionsPage;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class FilesTest {
     private WebDriver driver;
@@ -15,4 +23,37 @@ public class FilesTest {
     public void afterMethod() {
         driver.quit();
     }
+
+    @Test
+    public void checkFileDownloading() {
+        new HomePage()
+                .open()
+                .clickLoginButton();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://guest:welcome2qauto@qauto.forstudy.space/panel/garage");
+        new GaragePage()
+                .clickInstructionsButton();
+        new InstructionsPage()
+                .clickDownloadButton();
+        Assert.assertTrue(Files.exists(new InstructionsPage().getPDFFilePath()));
+    }
+
+    @Test
+    public void checkWritingAvailableCarsInFile() throws IOException {
+        new HomePage()
+                .open()
+                .clickLoginButton();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://guest:welcome2qauto@qauto.forstudy.space/panel/garage");
+        new GaragePage()
+                .clickInstructionsButton();
+        try {
+            new InstructionsPage()
+                    .clickButtonCarOption()
+                    .GetElementsAndWriteInFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Assert.assertTrue(new InstructionsPage().readFile());
+    }
 }
+
