6c07fdaa2e34970988915f207729ce6707db664f
Dmytro Kulish <mitiakylish2@gmail.com>
2025-03-24 21:02:06 +0200

    Added LoginTest and BrowserFactory

--- src/main/java/patterns/pageobject/BrowserFactory.java
+++ src/main/java/patterns/pageobject/BrowserFactory.java
@@ -1,22 +1,46 @@
 package patterns.pageobject;
-import com.codeborne.selenide.WebDriverRunner;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BrowserFactory {
-private static WebDriver driver;
+    private static WebDriver driver;
     public static void createDriver(String browserName) {
         switch (browserName.toLowerCase()) {
             case "chrome":
-                driver = new ChromeDriver();
+                WebDriverManager.chromedriver().setup();
+                Map<String, Object> prefs = new HashMap<>();
+                prefs.put("profile.default_content_settings.popups", 0);
+                prefs.put("download.default_directory", "C:\\Users\\User\\IdeaProjects\\untitled10\\target");
+                ChromeOptions options = new ChromeOptions();
+                options.setExperimentalOption("prefs", prefs);
+//                driver = new ChromeDriver(options);
+                try {
+                    driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
                 driver.manage().window().maximize();
-                WebDriverRunner.setWebDriver(driver);
                 break;
 
             case "firefox":
-                driver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                try {
+                    driver = new RemoteWebDriver(new URL("http://localhost:4444"), firefoxOptions);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+                driver.manage().window().maximize();
                 break;
+
             case "edge":
                 driver = new EdgeDriver();
                 break;
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import patterns.pageobject.BrowserFactory;
+import patterns.pageobject.HomePage;
+
 public class LoginTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    @Parameters("browser")
+    public void setup(String browser) {
+        BrowserFactory.createDriver(browser);
+        driver = BrowserFactory.getDriver();
+    }
+    @AfterMethod
+    public void teardown() {
+        BrowserFactory.tearDown();
+    }
+
+    @Test
+    public void checkLoginUser(){
+        driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
+        new HomePage()
+                .clickSignInButton()
+                .setEmail("test@hillel.ua")
+                .setPassword("1111")
+                .clickLoginButton();
+        Assert.assertTrue(new HomePage().getAlertMessage().isDisplayed());
+    }
 }
